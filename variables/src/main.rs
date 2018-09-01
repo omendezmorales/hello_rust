ca187b56a60e409ac26e258c7fe44e22676dd652
Orlando <mmoa33@hotmail.com>
2018-09-01 20:44:12 -0300

    3.1 Vars & mutability

---
+++ variables/src/main.rs
@@ -0,0 +1,21 @@
+// fn main() {
+    
+    // const MAX_POINTS: u32 = 100_000;
+
+    // let mut x=5;
+    // println!("the value of x is {}", x);
+    // x=6;
+    // println!("the value of x is {}", x);
+
+//}
+
+fn main() {
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+}
+
