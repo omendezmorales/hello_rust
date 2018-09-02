101d1d88f537fb35d3f9259f60ac7f9fc5ebadb8
Orlando <mmoa33@hotmail.com>
2018-09-01 22:12:36 -0300

    3.3 Functions

---
+++ functions/src/main.rs
@@ -0,0 +1,18 @@
+extern crate rand;
+use rand::Rng;
+
+fn main() {
+    println!("Hello, main!");
+    let x = rand::thread_rng().gen_range(1, 101);
+    another_function(x, x*x);
+    let f = five();
+    println!("callings five-> {}", f);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn another_function(x:i32, y:i32) {
+    println!("got this as input arguments {},{}.", x, y);
+}
