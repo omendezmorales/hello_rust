f06d222c0347b8189ab645b2a4dc7f179582d1c9
Orlando <mmoa33@hotmail.com>
2018-09-01 21:27:58 -0300

    Added some more variable examples

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -10,12 +10,18 @@
 //}
 
 fn main() {
+    let guess: u32 = "42".parse().expect("Not a number!");
     let x = 5;
-
+    let y: f32 = 3.0;
     let x = x + 1;
-
     let x = x * 2;
-
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+    let index = 100;
     println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of tuple is: {},{},{}", tup.0, tup.1,tup.2);
+    println!("The 1st value of months is: {}", months[index]); //interesting error message when running it.
 }
 
