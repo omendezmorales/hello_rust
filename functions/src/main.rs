a0abbebfbf03884aa365f1274f79772ab6d8c441
Orlando <mmoa33@hotmail.com>
2018-09-23 16:17:38 -0300

    Played with statement(s), but statements don’t evaluate to a value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -4,9 +4,10 @@ use rand::Rng;
 fn main() {
     println!("Hello, main!");
     let x = rand::thread_rng().gen_range(1, 101);
-    another_function(x, x*x);
+    //another_function(x, x*x);
     let f = five();
     println!("callings five-> {}", f);
+    //let x = (let y = 6);
 }
 
 fn five() -> i32 {
