f63dab858bc5ffc3997706c75b641142c83fb996
Orlando Mendez <mmoa33@hotmail.com>
2019-11-25 12:01:10 -0300

    Added can_hold method with more arguments

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -18,6 +18,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 
@@ -54,7 +58,10 @@ let user4 = build_user(String::from("mmoa33@hotmail.com"), String::from("orco"))
 // println!("User's 4 email: {}, user-id: {}", user4.email, user4.username);
 
 let rect1 = Rectangle { width: 30, height: 50 };
-let rect2 = (30,50);
+let rect2 = Rectangle { width: 10, height: 40 };
+let rect3 = Rectangle { width: 60, height: 45 };
+
+let rect2_t = (30,50);
     println!(
         "The area of the rectangle is {} square pixels, using method syntax",
         // area(&rect1)
@@ -62,10 +69,13 @@ let rect2 = (30,50);
     );
 
     println!("rect1 is {:?}", rect1);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
     
     println!(
         "The area of the rectangle is {} using a tuple.",
-        area_t(rect2)
+        area_t(rect2_t)
     );
 
 
