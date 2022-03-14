472c1186b463086c75cf1ccd4fe7bd43b53166c8
Orlando Mendez <mmoa33@hotmail.com>
2022-03-14 15:52:14 -0300

    Separating Modules into Different Files (sec.7.5)

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,18 +1,4 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 fn serve_order() {}
 
@@ -42,7 +28,7 @@ mod back_of_house {
     fn cook_order() {}
 }
 
-use crate::front_of_house::hosting
+use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     // Absolute path
