67c0df64184503f52cb0efa6101cce1562614dd2
Orlando Mendez <mmoa33@hotmail.com>
2022-05-12 14:17:00 -0400

    adding functions for using hashmaps

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut v = vec![1, 2, 3, 4, 5];
     //let first = &v[0];
@@ -13,11 +15,46 @@ fn main() {
     // let does_not_exist = &v[100];
     let does_not_exist = v.get(100);
     //        println!("The first element is: {}", first)
-    show_strings();
+    // show_strings();
+    println!("Calling hash map functions >>");
+    
+    print_hm(create_hm());
+    println!("Calling hash map update function >>");
+    update_hm();
+    println!("Updating a Value Based on the Old Value");
+    update_hm_for_wordcount();
+}
+fn update_hm_for_wordcount(){
+// TODO: LInk this code with how to read a file -see Section 12.2 in book-,
+//  and play with the idea of setting up a WordCloud in Rust
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+}
+
+fn update_hm(){
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    println!("prior to update> {:?}", scores);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("after update> {:?}", scores);
+    println!("Inserting a Value If the Key Has No Value, i.e. using entry method");
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!(" {:?}", scores);
 }
 
 fn show_strings() {
-    let s = String::new();
+    let _s = String::new();
 
     let data = "initial contents";
 
@@ -42,5 +79,22 @@ fn iterate_string_elements(s: String) {
 
     for b in s.bytes() {
         print!(" {}", b);
+    }
+}
+
+fn create_hm()-> HashMap<String, i32> {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let _score = scores.get(&team_name);
+    return scores;
+}
+
+fn print_hm(scores: HashMap<String, i32>){
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
     }
 }
