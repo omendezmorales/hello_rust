6534dd21b162b3aa0daba25f8e7c09871bd5433c
Orlando Méndez <mmoa33@hotmail.com>
2023-11-23 15:35:25 -0300

    feature to read URL from command line

--- url-shortener/src/main.rs
+++ url-shortener/src/main.rs
@@ -1,8 +1,25 @@
-use urlshortener::{client::UrlShortener,  providers::Provider};
+use std::env;
+use url::{Host, Url};
+use urlshortener::{client::UrlShortener, providers::Provider};
 
 fn main() {
-    let us = UrlShortener::new().unwrap();
-    let short_url = us.generate("https://1drv.ms/i/s!AsDcb1ONRmihnnRlWmRjGr7zVXAH?e=lXpanY", &Provider::IsGd );
-    assert!(short_url.is_ok());
-    println!("short url generated> {}",short_url.unwrap());
+    let args: Vec<String> = env::args().collect();
+
+    let input_url = &args[1];
+    println!("generating short url for {}", input_url.clone());
+    if validate_url(input_url.clone()) {
+        let us = UrlShortener::new().unwrap();
+        let short_url = us.generate(input_url, &Provider::IsGd);
+        assert!(short_url.is_ok());
+        println!("short url generated> {}", short_url.unwrap());
+    } else {
+        println!("invalid input url {}", input_url);
+    }
+}
+
+fn validate_url(a_url: String) -> bool {
+    // let issue_list_url = Url::parse(a_url).ok()?;
+    // issue_list_url.scheme() == "https"
+     //  && !issue_list_url.cannot_be_a_base());
+     return true
 }
