d348deffd91fde8ff52910ef4667da96f24cf044
Your Name <you@example.com>
2025-09-02 17:45:28 -0400

    restructure into fuctions to follow subsections of ch. 9 (error handling)

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,34 @@
+use std::{
+    fs::{self, File},
+    path::Path,
+};
+
+use std::error::Error;
+
 fn main() {
     // panic!("crash and burn");
+    open_file();
+    panic_show();
+}
+
+fn print_file(file_name: &str) -> Result<(), Box<dyn Error>> {
+    let message: String = fs::read_to_string(file_name)?;
+    println!("this is the contents of {}: {}", file_name, message);
+    Ok(())
+}
+fn open_file() {
+    let file_path = "hello.txt";
+    let greeting_file_result = File::open(file_path);
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file '{}'  {error:?}", file_path),
+    };
+    let path = Path::new(file_path);
+    print_file(path.file_name().unwrap().to_str().unwrap());
+}
+
+fn panic_show() {
     let v = vec![1, 2, 3];
 
     v[99];
