31711e010459de83057ea12f2c8d6c4037edf4b9
Your Name <you@example.com>
2025-09-08 11:46:51 -0300

    a bit  of pretty printing and unused vars compliance

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -13,19 +13,19 @@ fn main() {
 
 fn print_file(file_name: &str) -> Result<(), Box<dyn Error>> {
     let message: String = fs::read_to_string(file_name)?;
-    println!("this is the contents of {}: {}", file_name, message);
+    println!("this is the contents of {}:\n {}", file_name, message);
     Ok(())
 }
 fn open_file() {
     let file_path = "hello.txt";
     let greeting_file_result = File::open(file_path);
 
-    let greeting_file = match greeting_file_result {
+    let _greeting_file = match greeting_file_result {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file '{}'  {error:?}", file_path),
     };
     let path = Path::new(file_path);
-    print_file(path.file_name().unwrap().to_str().unwrap());
+    let _ = print_file(path.file_name().unwrap().to_str().unwrap());
 }
 
 fn panic_show() {
