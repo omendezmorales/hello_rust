aede2c8c2f5651dcaf12c46698b3153c3532a0ab
Orlando Méndez <mmoa33@hotmail.com>
2025-10-03 08:23:23 -0300

    added some etiquette to output and testing signed commit

--- public_ip_demo/src/main.rs
+++ public_ip_demo/src/main.rs
@@ -2,6 +2,8 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    
+    println!("This is the public IP provided by your ISP");
     // Perform my public IP address lookup
     let result = public_ip_address::perform_lookup(None).await?;
     println!("{}", result);
