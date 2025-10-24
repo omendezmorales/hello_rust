6578b9536ee58e57ba649bb35c077d7c49fd6046
Orlando Méndez <mmoa33@hotmail.com>
2025-10-24 12:18:55 -0300

    removed the urlshortener crate since lately is been breaking, and replacing it with reqwest

--- url-shortener/src/main.rs
+++ url-shortener/src/main.rs
@@ -1,8 +1,6 @@
 use std::env;
-use url::Url;
-
 use clipboard::{ClipboardContext, ClipboardProvider};
-use urlshortener::{client::UrlShortener, providers::Provider};
+use url_shortener::validate_url;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,20 +8,31 @@ fn main() {
     let input_url = &args[1];
     println!("generating short url for {}", input_url.clone());
     if validate_url(input_url.clone()).is_some() {
-        let us = UrlShortener::new().unwrap();
-        let short_url = us.generate(input_url, &Provider::IsGd);
-        assert!(short_url.is_ok());
-        println!("short url generated> {:?}", short_url.clone().unwrap());
-        let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-        ctx.set_contents(short_url.to_owned().expect("REASON"))
-            .unwrap();
-        println!("url copied!");
+        let api_url = format!("https://tinyurl.com/api-create.php?url={}", 
+                              urlencoding::encode(input_url));
+        let client = reqwest::blocking::Client::new();
+        let short_url = client.get(&api_url).send();
+        
+        match short_url {
+            Ok(response) => {
+                match response.text() {
+                    Ok(url) => {
+                        println!("short url generated> {}", url);
+                        let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+                        ctx.set_contents(url).unwrap();
+                        println!("url copied!");
+                    }
+                    Err(e) => {
+                        eprintln!("failed to read response: {:?}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("failed to generate short url: {:?}", e);
+            }
+        }
     } else {
         println!("invalid input url {}", input_url);
     }
 }
 
-pub fn validate_url(a_url: String) -> Option<bool> {
-    let issue_list_url = Url::parse(&a_url).ok()?;
-    Some(issue_list_url.scheme() == "https" && !issue_list_url.cannot_be_a_base())
-}
