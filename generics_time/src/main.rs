99452009576d8745bc2cc24a5201428d5d28ec7f
Orlando Méndez <mmoa33@hotmail.com>
2025-12-19 15:29:07 -0300

    added a mixup method for the Point struct

--- generics_time/src/main.rs
+++ generics_time/src/main.rs
@@ -9,9 +9,19 @@ fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
 
     largest
 }
-struct Point<T, U> {
-    x: T,
-    y: U,
+
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
 
 fn main() {
@@ -29,4 +39,12 @@ fn main() {
     let _both_integer = Point { x: 5, y: 10 };
     let _both_float = Point { x: 1.0, y: 4.0 };
     let _integer_and_float = Point { x: 5, y: 4.0 };
+
+    // calling mixed up generics
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
