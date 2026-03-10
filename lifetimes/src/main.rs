b8e4ae6755debd3412f53e7fe5e61d1e60d75d30
Orlando Méndez <mmoa33@hotmail.com>
2026-03-10 18:00:48 -0300

    including lifetime example in struct definition

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,12 +1,28 @@
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() { x } else { y }
 }
+
 fn main() {
     let string1 = String::from("long string is long");
-
+    let result;
     {
         let string2 = String::from("xyz");
-        let result = longest(string1.as_str(), string2.as_str());
+        result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is: '{result}'");
     }
+
+    let novel= String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+   println!("The 1st sentence is: '{:#?}'", i);
+
+ }
+
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
+
